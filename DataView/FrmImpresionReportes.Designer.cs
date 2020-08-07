2abbb1882f5ec5bf7abe03e1e5f9bc1463e34fb1
Josue <josue7114@gmail.com>
2020-08-07 16:01:20 -0600

    Actualizacion de impresion de reportes

---
+++ DataView/FrmImpresionReportes.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DataView
+{
+    partial class FrmImpresionReportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Location = new System.Drawing.Point(12, 12);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(776, 426);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // FrmImpresionReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer);
+            this.Name = "FrmImpresionReportes";
+            this.Text = "Reportes";
+            this.Load += new System.EventHandler(this.FrmImpresionReportes_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+    }
+}
---
+++ DataView/FrmImpresionReportes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataView
+{
+    public partial class FrmImpresionReportes : Form
+    {
+        public FrmImpresionReportes()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmImpresionReportes_Load(object sender, EventArgs e)
+        {
+            this.reportViewer.RefreshReport();
+        }
+    }
+}
